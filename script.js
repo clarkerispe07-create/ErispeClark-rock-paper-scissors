7ba4c53f66f1d276e2bd2d9c4dd6f68516323315
Clark Dee <263336227+clarkerispe07-create@users.noreply.github.com>
2026-04-23 17:06:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let wins = 0;
+let losses = 0;
+let ties = 0;
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * 3)];
+}
+
